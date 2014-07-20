ec592ef4dfe12a886807fbee5d7d0d334a62ac5f
Frank Duan <hotarubi.xp@gmail.com>
2014-07-20 10:14:18 +1000

    add next_moves to find all possible routes based on current square

--- lib/knight_move.rb
+++ lib/knight_move.rb
@@ -1,6 +1,8 @@
 class KnightMove
+  MAX_MOVES = 6.freeze
   VERTICAL_RANGE = ('a'..'h').freeze
   HORIZONTAL_RANGE = (1..8).freeze
+  ORIENTATIONS = [1, 2, -1, -2].permutation(2).to_a.select{|a,b| a.abs != b.abs }.freeze
 
   class OutOfChessboard < StandardError
     def initialize(square)
@@ -27,9 +29,29 @@ class KnightMove
   def to_target
   end
 
+  def next_moves
+    ORIENTATIONS.map { |v, h|
+      [move_vertically(@current[0], v), move_horizontal(@current[1], h)]
+    }.select { |move|
+      square_valid?(move) && ! @path.include?(move)
+    }
+  end
+
   protected
   def square_valid?(square)
     VERTICAL_RANGE.cover?(square[0]) &&
       HORIZONTAL_RANGE.cover?(square[1])
+  end
+
+  def maxed?
+    @path.size >= MAX_MOVES
+  end
+
+  def move_horizontal(h_num, step)
+    h_num + step
+  end
+
+  def move_vertically(v_char, step)
+    (v_char.ord + step).chr
   end
 end
